70f491d84763368fb14a024bf41cb708020ea1ab
Gustavo Martins <gustavo_mts2005@hotmail.com>
2021-05-20 08:35:23 -0300

    Updated the index page

--- System/PHP/index.php
+++ System/PHP/index.php
@@ -4,7 +4,6 @@
         <meta charset="UTF-8">
         <link type="image/x-icon" rel="shortcut icon" href="../IMG/logo-icone.ico">
         <link rel="stylesheet" type="text/css" href="../CSS/index.css">
-        <link rel="stylesheet" type="text/css" href="../CSS/login.css">
     </head>
     <body>
         <header>
@@ -19,24 +18,24 @@
         <?php } ?>
 
 
-        <form name="tela-inicial" method="POST" id="tela-inicial" action="../PHP/Login.php">
+        <form name="tela-inicial" method="POST" id="formTelaInicial" action="../PHP/Login.php">
             <?php if(isset($_SESSION['invalido'])) { ?>
                 <center><img src="../IMG/Logo_erro.png"></img></center>
             <?php } else { ?>
                 <center><img src="../IMG/Logo.png"></img></center>
             <?php } unset($_SESSION['invalido']); ?>
 
-            <label for="email">E-mail</label>
-            <input type="email" name="e-mail" class="inputs" size="22" maxlength="50" autofocus required>
+            <label class="lblLogin">E-mail</label>
+            <input type="email" name="e-mail" class="inputs LoginInputs" size="22" maxlength="50" autofocus required>
 
-            <label for="senha">Senha</label>
-            <input type="password" name="senha" class="inputs" size="22" maxlength="10" required>
+            <label class="lblLogin">Senha</label>
+            <input type="password" name="senha" class="inputs LoginInputs" size="22" maxlength="10" required>
 
             <span class="grupo_botoes">
                 <input type="submit" id="Entrar" value="Entrar">
             </span>
-            <a href="#">Esqueci minha senha</a>
-            <a href="FormCadastroPessoa.php">Criar uma conta</a>
+            <a class="loginLinks" href="#">Esqueci minha senha</a>
+            <a class="loginLinks" href="FormCadastroPessoa.php">Criar uma conta</a>
         </form>
     </body>
 </html>
